682328950b712c55be945b77bbe8a75836702281
Demis Meneghetti <demismeneghetti@msn.com>
2013-06-12 21:52:05 -0300

    Comentando exercícios para ocultar erros do projeto

--- src/exercicio_11/Teste_11_6.java
+++ src/exercicio_11/Teste_11_6.java
@@ -8,24 +8,24 @@ public class Teste_11_6 {
 
 	public static void main(String[] args) {
 
-		try{
-			objeto.metodoQuePodeLancarIOeSQLException();
-		}catch (IOException e){
-			//....
-		}catch (SQLException e){
-			//....
-		}
+//		try{
+//			objeto.metodoQuePodeLancarIOeSQLException();
+//		}catch (IOException e){
+//			//....
+//		}catch (SQLException e){
+//			//....
+//		}
 	}
 	
 	public void abre1(String arquivo) throws IOException, SQLException {
 		//....
 	}
 	
-	public void abre2(String arquivo) throws IOException{
-		try {
-			objeto.metodoQuePodeLancarIOeSQLException();
-		} catch (SQLException e) {
-			// TODO: handle exception
-		}
-	}
+//	public void abre2(String arquivo) throws IOException{
+//		try {
+//			objeto.metodoQuePodeLancarIOeSQLException();
+//		} catch (SQLException e) {
+//			// TODO: handle exception
+//		}
+//	}
 }
